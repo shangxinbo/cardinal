1b487bc63fc936e0fc33bc4b4c8fe25f1af48ee3
shangxinbo <314911714@qq.com>
2016-12-14 19:13:00 +0800

    no message

--- app.js
+++ app.js
@@ -4,22 +4,11 @@
  * AUTHOR shangxinbo
  */
 
-const process = require('process')
-const program = require('commander')
 
 const jet = require('jet');
 
-const defaultHost = '0.0.0.0'
-const defaultPort = 9527
-
-program
-    .option('-h, --host <host>', `host address, default value: ${defaultHost}`)
-    .option('-p, --port <port>', `port number, default value: ${defaultPort}`, parseInt)
-    .parse(process.argv)
-
-
-const host = program.host || defaultHost
-const port = program.port || defaultPort
+const host = '127.0.0.1';
+const port = 9527;
 
 jet.listen(port, host, () => {
     console.log(`Listening on ${host}:${port}...`)
