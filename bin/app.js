ae9f89c798d343e6abf5a1f28611f1a62ef38b8b
shangxinbo <314911714@qq.com>
2017-01-04 19:12:26 +0800

    完成优化服务器选择

--- bin/app.js
+++ bin/app.js
@@ -15,37 +15,51 @@ spider.update();
 
 let tcpPorts = tcp.createServer();
 //TODO 选择合适的tcp代理
-for (var i = 0; i < tcpPorts.length; i++) {
-    var socksConfig = {
+let filters = [];
+for (let i = 0; i < tcpPorts.length; i++) {
+    let socksConfig = {
         proxyHost: '127.0.0.1',
         proxyPort: tcpPorts[i],
         auths: [Socks.auth.None()]
     };
-
-    var req = http.get({
+    let req = http.get({
         hostname: 'google.com',
         port: 80,
         agent: new Socks.HttpAgent(socksConfig)
     }, function (res) {
-        console.log(res.statusCode, res.headers);
+        if (res.statusCode == 200 || res.statusCode == 302) {
+            filters.push(tcpPorts[i]);
+        }
     });
-    req.setTimeout(1000,function(){
+    req.on('error', function () {
+        logger.error('TCP hang up unexpacted');
+        req.end();
+    });
+    req.setTimeout(1000, function () {
         req.abort();
     });
 }
 
+start();
+function start() {
+    setTimeout(function () {
+        if (filters.length > 0) {
+            let httpPorts = mhttp.createServer(filters);
+            let pacServer = pac.createServer(httpPorts);
 
-
-// let httpPorts = mhttp.createServer(socksPorts);
-// let pacServer = pac.createServer(httpPorts);
-
-// // set browser proxy auto config url
-// // on windows platform chrome://settings/ ——>  更改代理服务器 ——> 连接 ——> 局域网设置 ——>使用自动配置脚本
-// var cmd = 'reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v AutoConfigURL /t REG_SZ /d "http://' + config.host + ':' + config.pacPort + '/proxy.pac" /f';
-// var child = exec(cmd, function (err, stdout, stderr) {
-//     if (err) throw err;
-//     logger.status('set PAC success');
-// });
-// process.on('uncaughtException', function (err) {
-//     logger.error('uncaughtException' +err);
-// });
+            // set browser proxy auto config url
+            // on windows platform chrome://settings/ ——>  更改代理服务器 ——> 连接 ——> 局域网设置 ——>使用自动配置脚本
+            var cmd = 'reg add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" /v AutoConfigURL /t REG_SZ /d "http://' + config.host + ':' + config.pacPort + '/proxy.pac" /f';
+            var child = exec(cmd, function (err, stdout, stderr) {
+                if (err) throw err;
+                logger.status('set PAC success');
+            });
+            process.on('uncaughtException', function (err) {
+                //TODO recovery browser proxy auto url
+                logger.error('uncaughtException' + err);
+            });
+        } else {
+            start();
+        }
+    }, 1000);
+}
