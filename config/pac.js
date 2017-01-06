37cc00eb9922edebd2b86f72c04351e6e41a1b44
shangxinbo <314911714@qq.com>
2017-01-06 11:39:51 +0800

    本地请求或内网请求直连

--- config/pac.js
+++ config/pac.js
@@ -1,6 +1,10 @@
 
 function FindProxyForURL(url, host){
-    if(inNets(host)){
+    if(isPlainHostName(host)){
+        return "DIRECT;";
+    }else if(isInNet(host, "10.0.0.0", "255.0.0.0") ||isInNet(host, "172.16.0.0",  "255.240.0.0") ||isInNet(host, "192.168.0.0",  "255.255.0.0") ||isInNet(host, "127.0.0.0", "255.255.255.0")) {
+        return "DIRECT";
+    }else if(inNets(host)){
         return "DIRECT;";
     }else{
         return proxy;
