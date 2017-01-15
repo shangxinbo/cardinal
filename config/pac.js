83e937809195cb6eeb169f658b166e3591ad8b0d
shangxinbo <314911714@qq.com>
2017-01-15 17:50:48 +0800

    format pac.js

--- config/pac.js
+++ config/pac.js
@@ -1,19 +1,19 @@
 
-function FindProxyForURL(url, host){
-    if(isPlainHostName(host)){
+function FindProxyForURL(url, host) {
+    if (isPlainHostName(host)) {
         return "DIRECT;";
-    }else if(isInNet(host, "10.0.0.0", "255.0.0.0") ||isInNet(host, "172.16.0.0",  "255.240.0.0") ||isInNet(host, "192.168.0.0",  "255.255.0.0") ||isInNet(host, "127.0.0.0", "255.255.255.0")) {
+    } else if (isInNet(host, "10.0.0.0", "255.0.0.0") || isInNet(host, "172.16.0.0", "255.240.0.0") || isInNet(host, "192.168.0.0", "255.255.0.0") || isInNet(host, "127.0.0.0", "255.255.255.0")) {
         return "DIRECT";
-    }else if(inNets(host)){
+    } else if (inNets(host)) {
         return "DIRECT;";
-    }else{
+    } else {
         return proxy;
     }
 }
 
-function inNets(host){
-    for(var i=0;i<ipsArr.length;i++){
-        if(isInNet(host,ipsArr[i].ip,ipsArr[i].mask)){
+function inNets(host) {
+    for (var i = 0; i < ipsArr.length; i++) {
+        if (isInNet(host, ipsArr[i].ip, ipsArr[i].mask)) {
             return true;
         }
     }
